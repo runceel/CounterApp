9fe12a592e58d57e03a2095022dbf68efe063652
Kazuki Ota <kaota@microsoft.com>
2022-05-30 19:56:15 +0900

    プロジェクト ファイルを追加します。

---
+++ CounterApp/CounterStateService.cs
@@ -0,0 +1,20 @@
+﻿namespace CounterApp;
+
+public interface ICounterStateService
+{
+    Task<int> GetCounterValueAsync();
+    Task SetCounterValueAsync(int value);
+}
+
+public class CounterStateService : ICounterStateService
+{
+    private int _value;
+    public Task<int> GetCounterValueAsync() => 
+        Task.FromResult(_value);
+
+    public Task SetCounterValueAsync(int value)
+    {
+        _value = value;
+        return Task.CompletedTask;
+    }
+}
